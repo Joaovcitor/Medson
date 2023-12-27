457d0146d43ee3b43b363f05e37603aa49514b88
João Victor <Neguchos96@gmail.com>
2023-12-27 08:19:20 -0300

    add bnt

--- codes/carrosel.js
+++ codes/carrosel.js
@@ -1,18 +1,38 @@
-var slideIndex = 0;
+// var slideIndex = 0;
 
-function carouselFinal() {
+// function carouselFinal() {
+//   var i;
+//   var x = document.getElementsByClassName("slide");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > x.length) {slideIndex = 1}
+//   x[slideIndex-1].style.display = "block";
+//   setTimeout(carouselFinal, 1500); // Change image every 2 seconds
+// }
+
+// carouselFinal()
+
+// TESTES
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("slide");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
-  setTimeout(carouselFinal, 1500); // Change image every 2 seconds
 }
-
-carouselFinal()
 
 
 var slideIndex = 0;
